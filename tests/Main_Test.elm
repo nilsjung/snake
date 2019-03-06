0496c444bced8f50b4d5eef49ec57ae228d5a9cb
Nils Jung <nils.m.jung@student.fh-kiel.de>
2019-03-06 17:51:27 +0100

    Justify Tests

--- tests/Main_Test.elm
+++ tests/Main_Test.elm
@@ -4,24 +4,32 @@ import Expect exposing (Expectation)
 import Test exposing (..)
 import Main
 
+import Point exposing (Point)
+
 
 suite : Test
 suite =
     describe "Snake Tests"
         [ describe "Main isCollusion"
-            [ test "returns true if point collates with list" <|
+            [ test "returns true if the point collides with the first element of the list" <|
                 \_ ->
                     let
-                        list = [{x = 100, y = 200}, {x = 101, y = 200}]
-                        point = {x = 101, y = 200}
+                        list = [Point 100 200, Point 101 200]
+                        point = Point 100 200
                     in
                         Expect.true "Expect the point to be in the list" (Main.isCollusion list point)
-            ,
-             test "returns false if point is not part of the list" <|
+            , test "returns false of another element of the list is equal" <|
                 \_ ->
                     let
-                        list = [{x = 100, y = 200}, {x = 101, y = 200}]
-                        point = {x = 103, y = 200}
+                        list = [Point 100 200, Point 101 200]
+                        point = Point 101 200
+                    in
+                        Expect.false "Expect the head not to be equal" (Main.isCollusion list point)
+            , test "returns false if point is not part of the list" <|
+                \_ ->
+                    let
+                        list = [Point 100 200, Point 101 200]
+                        point = Point 101 201
                     in
                         Expect.false "Expect the point to be not in the list" (Main.isCollusion list point)
             ]
